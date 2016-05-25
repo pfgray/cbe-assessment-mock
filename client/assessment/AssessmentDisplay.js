11e04d5f3540b16f3c7e82a9dbf728e9b25ec972
Paul Gray <pgray@learningobjects.com>
2016-05-25 11:28:23 -0400

    Adding error-hnadling in parser

--- client/assessment/AssessmentDisplay.js
+++ client/assessment/AssessmentDisplay.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import AssessmentService from './AssessmentService';
 
+const renderResult = result => (
+  <div>YAY: {JSON.stringify(result)}</div>
+);
 
-const AssessmentDisplay = ({result}) => {
+const renderError = err => (
+  <div>ERROR: {JSON.stringify(err)}</div>
+);
+
+const AssessmentDisplay = ({ result }) => {
   return (
     <div className='assessment-result-container'>
       <div className='assessment-result-header'>
         <Link className='btn btn-default' to={'/assessment/source'}>{'{} source'}</Link>
       </div>
-      <div>
-        hmmm {result}
-      </div>
+      {AssessmentService.parseResult(result, renderResult, renderError)}
     </div>
   );
-}
+};
 
 export default AssessmentDisplay;
